534092be55b428c8d0824ab92a82e86b69712156
SergeyMakhotkin <ser9box@mail.ru>
2019-09-14 20:16:33 +0300

    не доделал автоматичесское меню. проблема с картинками

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_DIR = os.path.join(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = [
- #   os.path.join(BASE_DIR, 'static')
-#]
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static/')
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media/')
--- geekshop/urls.py
+++ geekshop/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from mainapp.views import main, products, contacts, product_1, product_2, product_3
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', main, name='main'),
@@ -26,3 +28,7 @@ urlpatterns = [
     path('prod_3/', product_3, name='product_6420'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
