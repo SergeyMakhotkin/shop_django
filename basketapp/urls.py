a47a15940f0c94210a3c90c69b1dcc2062d68689
SergeyMakhotkin <ser9box@mail.ru>
2019-09-24 14:07:57 +0300

    Доделал задание №6

--- basketapp/urls.py
+++ basketapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import add, remove, basket
+from .views import add, remove, basket, edit, remove_slot
 
 app_name = 'basketapp'
 
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', basket, name='read'),
     path('add/<int:product_pk>/', add, name='add'),
     path('remove/<int:product_pk>', remove, name='remove'),
+    path('remove_slot/<int:slot_pk>', remove_slot, name='remove_slot'),
+    path('edit/<int:pk>/', edit, name='edit')
 ]
--- basketapp/views.py
+++ basketapp/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import HttpResponseRedirect, get_object_or_404, render, reverse
+from django.shortcuts import HttpResponseRedirect, get_object_or_404, render, reverse, HttpResponse
 from .models import BasketSlot
 from mainapp.models import Product
 from django.contrib.auth.decorators import login_required
+from django.template.loader import render_to_string
+from django.http import JsonResponse
 
 @login_required
 def basket(request):
@@ -32,7 +34,7 @@ def add(request, product_pk):
 @login_required
 def remove(request, product_pk):
     if 'login' in request.META.get('HTTP_REFERER'):
-        return HttpResponseRedirect(reverse('products:product', args=[pk]))
+        return HttpResponseRedirect(reverse('products:product', args=[product_pk]))
 
     product = get_object_or_404(Product, pk=product_pk)
     basket_slot = BasketSlot.objects.filter(user=request.user, product=product).first()
@@ -45,3 +47,34 @@ def remove(request, product_pk):
             basket_slot.save()
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def remove_slot(request, slot_pk):
+    if 'login' in request.META.get('HTTP_REFERER'):
+        return HttpResponseRedirect(reverse("basket"))
+
+    basket_slot = BasketSlot.objects.filter(user=request.user, id=slot_pk).first()
+
+    if basket_slot:
+        basket_slot.delete()
+
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+@login_required
+def edit(request, pk):
+    print(pk)
+    print(request.GET.get('quantity'))
+    basket_slot = get_object_or_404(BasketSlot, pk=pk)
+    quantity = int(request.GET.get('quantity'))
+    if quantity > 0:
+        basket_slot.quantity = quantity
+        basket_slot.save()
+    else:
+        basket_slot.delete()
+    basket_items = BasketSlot.objects.filter(user=request.user)
+    context = {'basket_items': basket_items,}
+    result = render_to_string('includes/inc_basket_list.html', context)
+
+    return JsonResponse({'result': result})
